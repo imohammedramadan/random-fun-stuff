b72a8f4a429fb9a2741a99d2d29b62aa73b73810
Mohammed Ramadan <mohammed.ramadan1474@gmail.com>
2021-07-15 16:22:53 +0200

    Updated the script with more browsers and conditions added an if condition for default port of 5000 or different port added more browsers displayed a browser started message after a browser starts

--- all browsers testing.bat
+++ all browsers testing.bat
@@ -1,14 +1,31 @@
-echo off
-set PFL1=C:\Program Files
-set PFL2=C:\Program Files (x86)
-set /p port=Enter port:
-set url=http://localhost:%port%/
-set durl=Your URL is %url%
-echo %durl%
-start "firefox" "%PFL1%\Mozilla Firefox\firefox.exe" %url%
-start "firefox dev" "%PFL1%\Firefox Developer Edition\firefox.exe" %url%
-@REM if you have 2 different versions of the same browser, make sure to add the path to each of them like firefox above this comment
-start "chrome" chrome.exe %url%
-start "edge" msedge.exe %url%
-start "Internet Explorer" iexplore.exe %url%
-start "Opera" opera.exe
+@echo off
+SETLOCAL EnableDelayedExpansion
+set PFiles64=C:\Program Files
+set PFiles86=C:\Program Files (x86)
+:PROMPT
+    SET /P useDefaultPort= Do you want to use the default port (5000)? [Y/N]
+    IF /I %useDefaultPort% EQU Y ( GOTO RUNBROWSERSDEFAULT ) else ( GOTO ENTERPORT )
+:ENTERPORT
+    set /P port=Enter port:
+    set targetUrl=http://localhost:%port%/
+    GOTO RUNBROSWERS
+:RUNBROWSERSDEFAULT
+    set targetUrl=http://localhost:5000/
+:RUNBROSWERS
+    echo %targetUrl%
+    start "Chrome" "%PFiles64%\Google\Chrome\Application\chrome.exe" %targetUrl%
+        Echo Chrome Started
+    @REM start "Firefox" "%PFiles64%\Mozilla Firefox\firefox.exe" %url%
+        @REM Echo Firefox Started
+    @REM start "Edge" msedge.exe %url%
+        @REM Echo Edge Started
+    @REM start "Chrome dev" chrome.exe %url%
+        @REM Echo Chrome dev Started
+    @REM start "Firefox dev" "%PFiles64%\Firefox Developer Edition\firefox.exe" %url%
+        @REM Echo Firefox dev Started
+    @REM if you have 2 different versions of the same browser, make sure to add the path to each of them like firefox above this comment
+    @REM start "Internet Explorer" iexplore.exe %url%
+        @REM Echo Internet Explorer Started
+    @REM start "Opera" opera.exe
+        @REM Echo Opera Started
+Pause
